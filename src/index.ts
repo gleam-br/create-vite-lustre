f41af82587d9d33aca1e95b37b3b4f023c02bdff
salespaulo <salespaulo08@gmail.com>
2025-11-17 23:09:15 -0300

    fix bugs after tests, looks good

--- src/index.ts
+++ src/index.ts
@@ -5,69 +5,62 @@
  */
 
 
-import { resolve, dirname, basename, join } from "path"
-import { copyFileSync, existsSync, mkdirSync, readdirSync, readFileSync, rmSync, statSync, writeFileSync } from "fs"
+import {
+  resolve,
+  dirname,
+  basename,
+  join,
+  sep
+} from "path"
+
+import {
+  copyFileSync,
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  readFileSync,
+  statSync,
+  writeFileSync
+} from "fs"
+
+import { fileURLToPath } from "url"
 
 import mri from "mri"
+
 import { execa } from "execa"
 
 import { name, version } from "../package.json"
-import { deepMerge, sortDependencies } from "./utils"
-import { fileURLToPath } from "url"
 
+import { deepMerge, sortDependencies } from "./utils"
+
+/** Default dest template directory */
 const DEFAULT_TARGET_DIR = `${name}-project`
 
-enum Template {
-  vanilla = 'vanilla',
-  vue = 'vue',
-  react = 'react',
-  react_compiler = 'react-compiler',
-  react_swc = 'react_swc',
-  preact = 'preact',
-  lit = 'lit',
-  svelte = 'svelte',
-  solid = 'solid',
-  qwik = 'qwik',
-}
-
-enum LogLevel {
-  none = "none",
-  trace = "trace",
-  debug = "debug",
-  info = "info",
-}
-
-enum PackageManager {
-  bun = "bun",
-  npm = "npm",
-  pnpm = "pnpm",
-  yarn = "yarn",
-}
-
+/**
+ * Options to create vite lustre project.
+ */
 interface Options {
   help: boolean, // show help
-  template: Template,
+  template: string, // see vite-create templates
   rolldown: boolean, // experimental rolldown
   overwrite: boolean // overwrite if already exist files
-  immediate: boolean, // install now deps
   typescript: boolean,  // template is *.ts
   bin: {
-    pm: PackageManager,
+    pm: string, // bun, npm, pnpm, yarn, etc
     gleam: string, // Where is gleam binary
   },
   dir: { // dirs
     cwd: string // process.cwd()
   },
   log: {
-    level: LogLevel // log level
+    level: string // "info" | "debug" | "trace" | "none"
     time: boolean // if put date time
   }
 }
 
-type Argv = Partial<Options>
-
-// New argv parser to mri lib
-//
+/**
+ * New argv parser to mri lib
+ */
 const newArgv = {
   boolean: [
     "help",
@@ -75,7 +68,7 @@ const newArgv = {
     "immediate",
     "rolldown",
     "typescript",
-    "log.time",
+    "log-time",
   ],
   alias: {
     h: "help",
@@ -86,33 +79,43 @@ const newArgv = {
   },
   string: [
     "template",
-    "bin.pm",
-    "bin.gleam",
-    "dir.cwd",
-    "log.level",
+    "bin-pm",
+    "bin-gleam",
+    "dir-cwd",
+    "log-level",
   ],
+  defualt: {
+    template: "vanilla",
+    help: false,
+    rolldown: false,
+    immediate: false,
+    overwrite: false,
+    typescript: false,
+  }
 }
 
+/**
+ * Main function
+ */
 async function main(argv: any): Promise<void> {
   // options from process.argv
-  const options = mri<Argv>(argv, newArgv)
-
+  const options = mri(argv, newArgv)
   // normalize options
+  const opts = newOpt(options);
   const {
     template,
     help,
     rolldown,
     overwrite,
-    immediate,
     typescript,
     bin: { pm, gleam },
     dir: { cwd },
     log: { level, time }
-  } = newOpt(options);
+  } = opts;
 
   // new log instance
   const log = logger(level, time)
-  log(`STARTUP OK ${version} !`)
+  log(`STARTUP v${version} OK !`)
 
   try {
     const args: string[] = [
@@ -134,10 +137,6 @@ async function main(argv: any): Promise<void> {
         : template
     )
 
-    if (immediate) {
-      args.push("-i")
-    }
-
     if (rolldown) {
       args.push("--rolldown")
     }
@@ -146,70 +145,120 @@ async function main(argv: any): Promise<void> {
       args.push("--overwrite")
     }
 
-    await runBin(log, cwd, pm, args)
-
     const targetDir = options._[0]
-      ? formatTargetDir(String(options._[0]))
+      ? normalizeTargetDir(String(options._[0]))
       : DEFAULT_TARGET_DIR
 
+    log(`:> targetDir ${targetDir}`)
+
+    if (existsSync(targetDir) && !isEmpty(targetDir) && !overwrite) {
+      throw new Error(`Target dir is not empty!\n'${cwd}${sep}${targetDir}'`)
+    }
+
+    // run create-vite
+    args.push(targetDir)
+    await runBin(log, cwd, pm, args)
+
     const src = resolve(fileURLToPath(import.meta.url), "../..", "template")
+    log(`:> srcDir ${src}`)
 
     const dest = join(cwd, targetDir)
-    const gleamName = basename(resolve(targetDir)).replace("-", "_")
+    log(`:> destDir ${dest}`)
+
+    const gleamName = basename(resolve(targetDir)).replaceAll("-", "_")
 
     // gleam new
-    await runBin(log, dest, gleam, [
+    const gleamNewArgs = [
+      "new",
       "--name",
       gleamName,
       "--skip-git",
       "--skip-github",
       "--template", "javascript",
       ".",
-    ])
+    ]
+    await runBin(log, dest, gleam, gleamNewArgs)
 
     // gleam add lustre
-    await runBin(log, dest, gleam, [
+    const gleamAddArgs = [
       "add",
       "lustre",
-    ])
+    ]
+    await runBin(log, dest, gleam, gleamAddArgs)
 
-    await copyFiles(src, dest)
+    await copyFiles(log, src, dest)
+
+    // gleam build
+    const gleamBuildArgs = [
+      "build",
+      "--target",
+      "javascript",
+    ]
+    await runBin(log, dest, gleam, gleamBuildArgs)
+
+    // pm install
+    const pmInstallArgs = [
+      "install",
+    ]
+    await runBin(log, dest, pm, pmInstallArgs)
+
+    // pm run build
+    const pmRunBuildArgs = [
+      "run",
+      "build",
+    ]
+    await runBin(log, dest, pm, pmRunBuildArgs)
+
+    log("FINISH OK !")
+    process.exit(0)
 
   } catch (err) {
-    log(`${JSON.stringify(err)}`, true)
+    log(`Create vite lustre template error!`, true)
+    console.error(err)
     process.exit(1)
   }
 }
 
-async function copyFiles(src: string, dest: string): Promise<void> {
+/**
+ * Copy template files to target dir
+ */
+async function copyFiles(log: any, src: string, dest: string): Promise<void> {
   const stats = statSync(src)
 
   if (stats.isDirectory()) {
     // skip node_module
     if (basename(src) === 'node_modules') {
+      log(`|> skip node_modules`)
       return
     }
 
-    // mkdir subdirectories and copy files
+    // subdirectories and copy files
+    log(`|> mkdir ${dest}`)
     mkdirSync(dest, { recursive: true })
+    log(`|>> OK !`)
 
+    log(`|> copy recursive`)
     for (const file of readdirSync(src)) {
-      copyFiles(resolve(src, file), resolve(dest, file))
+      await copyFiles(log, resolve(src, file), resolve(dest, file))
     }
 
+    log(`|>> copy recursive OK !`)
     return
   }
 
   const filename = basename(src)
+  log(`:> copy ${filename}`)
   const copiedPackageJson = await copyPackageJson(filename, src, dest)
 
   if (copiedPackageJson) {
+    log(`:> OK !`)
     return
   }
 
   const copiedVsCode = copyVsCodeFiles(filename, src, dest)
 
   if (copiedVsCode) {
+    log(`:> OK !`)
     return
   }
 
@@ -217,29 +266,17 @@ async function copyFiles(src: string, dest: string): Promise<void> {
   const copiedGit = copyGitIgnore(filename, src, dest)
 
   if (copiedGit) {
+    log(`:> OK !`)
     return
   }
 
   copyFileSync(src, dest)
+  log(`:> OK !`)
 }
 
-async function copyPackageJson(filename: string, src: string, dest: string) {
-  if (filename === 'package.json' && existsSync(dest)) {
-    // merge instead of overwriting
-    const existing = await import(dest);
-    const newPackage = await import(src);
-    // TODO: remove after test it
-    // const existing = JSON.parse(readFileSync(dest, 'utf8'))
-    // const newPackage = JSON.parse(readFileSync(src, 'utf8'))
-    const pkg = sortDependencies(deepMerge(existing, newPackage))
-
-    writeFileSync(dest, JSON.stringify(pkg, null, 2) + '\n')
-    return true
-  }
-
-  return false
-}
-
+/**
+ * Replace hidden files begin with '_' to '.'
+ */
 function replaceHidden(filename: string, src: string, dest: string): string {
   if (filename.startsWith('_')) {
     // rename `_file` to `.file`
@@ -249,6 +286,29 @@ function replaceHidden(filename: string, src: string, dest: string): string {
   return dest
 }
 
+/**
+ * Copy package.json
+ */
+async function copyPackageJson(filename: string, src: string, dest: string) {
+  if (filename === 'package.json' && existsSync(dest)) {
+    // merge instead of overwriting
+    // const existing = await import(dest);
+    // const newPackage = await import(src);
+    // TODO: remove after test it
+    const existing = JSON.parse(readFileSync(dest, 'utf8'))
+    const newPackage = JSON.parse(readFileSync(src, 'utf8'))
+    const pkg = sortDependencies(deepMerge(existing, newPackage))
+
+    writeFileSync(dest, JSON.stringify(pkg, null, 2) + '\n')
+    return true
+  }
+
+  return false
+}
+
+/**
+ * Copy .gitignore
+ */
 function copyGitIgnore(filename: string, src: string, dest: string): boolean {
   if (filename === '_gitignore' && existsSync(dest)) {
     // append to existing .gitignore
@@ -262,6 +322,9 @@ function copyGitIgnore(filename: string, src: string, dest: string): boolean {
   return false
 }
 
+/**
+ * Copy .vscode files
+ */
 function copyVsCodeFiles(filename: string, src: string, dest: string): boolean {
   if (filename === 'extensions.json' && existsSync(dest)) {
     // merge instead of overwriting
@@ -285,80 +348,185 @@ function copyVsCodeFiles(filename: string, src: string, dest: string): boolean {
   return false
 }
 
+/**
+ * Run bin executable with args from cwd and log process
+ */
 async function runBin(
   log: any,
   cwd: string,
   bin: string,
   args: string[]
 ): Promise<void> {
-
   log(`$ ${bin} ${args.join(" ")}`)
-  const res = await run(cwd, bin, args)
+  const { stdout } = await run(cwd, bin, args)
 
-  log(`$ ${res.stdout}${res.stderr}`)
+  if (stdout) {
+    console.log(stdout)
+  }
+
+  log(`:> RUN OK !`)
 }
 
+/**
+ * New normalized options
+ */
+function newOpt(options: any | undefined): Options {
+  const help = typeof options.help === "boolean"
+    ? options.help
+    : false
+  const rolldown = typeof options.rolldown === "boolean"
+    ? options.rolldown
+    : false
+  const overwrite = typeof options.overwrite === "boolean"
+    ? options.overwrite
+    : false
+  const pm = typeof options["bin-pm"] === 'string' && options["bin-pm"] !== ""
+    ? options["bin-pm"]
+    : getBinPmFromUserAgent()
+  const gleam = typeof options["bin-gleam"] === "string" && options["bin-gleam"] !== ""
+    ? options["bin-gleam"]
+    : "gleam"
+  const cwd = typeof options["dir-cwd"] === "string" && options["dir-cwd"] !== ""
+    ? options["dir-cwd"]
+    : process.cwd()
+  const level = typeof options["log-level"] === "string" && options["log-level"] !== ""
+    ? options["log-level"]
+    : "none"
+  const time = typeof options["log-time"] === "boolean"
+    ? options["log-time"]
+    : false
+
+  let template = typeof options.template === "string" && options.template !== ""
+    ? options.template
+    : "vanilla"
+  let typescript = typeof options.typescript === "boolean"
+    ? options.typescript
+    : false
+
+  if (template.endsWith("-ts")) {
+    template = template.slice(0, template.length - 3)
+    typescript = true
+  }
+
+  return {
+    template,
+    help,
+    rolldown,
+    overwrite,
+    typescript,
+    bin: {
+      pm,
+      gleam
+    },
+    dir: {
+      cwd,
+    },
+    log: {
+      level,
+      time,
+    }
+  }
+}
+
+/**
+ * Exec command in child process asynchronous.
+ *
+ * return e.g. Promise<{
+ *   command: "bun create vite --no-interactive --template vanilla create-vite-lustre-project",
+ *   escapedCommand: "bun create vite --no-interactive --template vanilla create-vite-lustre-project",
+ *   cwd: "C:\\work\\tools\\create-vite-lustre",
+ *   durationMs: 220.2958,
+ *   failed: false,
+ *   timedOut: false,
+ *   isCanceled: false,
+ *   isGracefullyCanceled: false,
+ *   isTerminated: false,
+ *   isMaxBuffer: false,
+ *   isForcefullyTerminated: false,
+ *   exitCode: 0,
+ *   stdout: "\u001b[90m└\u001b[39m  \u001b[31mOperation cancelled\u001b[39m\n",
+ *   stderr: "",
+ *   stdio: [ undefined, "\u001b[90m└\u001b[39m  \u001b[31mOperation cancelled\u001b[39m\n",
+ *     ""
+ *   ],
+ *   ipcOutput: [],
+ *   pipedFrom: [],
+ * }>
+ */
 async function run(cwd: string, bin: string, args: string[]): Promise<any> {
   return await execa(bin, args, { cwd, encoding: "utf8", timeout: 5000 })
 }
 
-function newOpt(options: any | undefined): Options {
-  // TODO: null to default normalize options
-  return {
-    template: Template.vanilla,
-    help: false,
-    rolldown: false,
-    immediate: false,
-    overwrite: false,
-    typescript: false,
-    bin: {
-      pm: PackageManager.bun,
-      gleam: "gleam"
-    },
-    dir: {
-      cwd: process.cwd(),
-    },
-    log: {
-      level: LogLevel.none,
-      time: false,
-    }
-  }
-}
+/**
+ * New log instance
+ */
+function logger(level: string, time = false) {
+  const isNone = level === "none"
+  const isTrace = level === "trace"
+  const isDebug = isTrace || level === "debug"
+  const isInfo = !isDebug
 
-export const logger = (level: string, time = false) => {
-  const isNone = level === "none";
-  const isTrace = level === "trace";
-  const isDebug = isTrace || level === "debug";
-  const isInfo = !isDebug;
-
-  // console.log(msg)
   return (msg: string, error = false): void => {
-    const isCmd = msg.startsWith("$ ");
+    const isCmd = msg.startsWith("$ ")
 
-    if ((isNone && !isCmd) || (!isTrace && msg.includes("skip"))) {
-      return;
+    if (isNone && !isCmd) {
+      return
     }
 
-    const prefix = msg.startsWith(":>") ? "[debug]" : "";
+    const prefixTrace = msg.startsWith("|>") ? "[trace]" : ""
+    const prefixDebug = msg.startsWith(":>") ? "[debug]" : ""
 
-    if (!isCmd && !error && isInfo && prefix !== "") {
-      return;
+    if (!isCmd && !error && (isDebug || isInfo) && prefixTrace !== "") {
+      return
     }
 
-    const prefixTime = time === true ? `${new Date().toISOString()}` : "";
+    if (!isCmd && !error && isInfo && prefixDebug !== "") {
+      return
+    }
+
+    const prefix = `${prefixTrace}${prefixDebug}`
+    const prefixTime = time === true ? `${new Date().toISOString()}` : ""
 
     if (error) {
-      console.error(`${prefixTime}[${name}]${prefix} ERROR | ${msg}`);
+      console.error(`${prefixTime}[${name}]${prefix} ERROR | ${msg}`)
     }
 
-    console.log(`${prefixTime}[${name}]${prefix} ${msg}`);
+    console.log(`${prefixTime}[${name}]${prefix} ${msg}`)
   }
 }
 
-function formatTargetDir(targetDir: string) {
+/**
+ * Normalize target dir, remove all '/'.
+ */
+function normalizeTargetDir(targetDir: string) {
   return targetDir.trim().replace(/\/+$/g, '')
 }
 
-// MAIN
-//
+/**
+ * Get binary package manager from user agent (process.env.npm_config_user_agent)
+ */
+function getBinPmFromUserAgent(): string {
+  const userAgent = process.env.npm_config_user_agent
+
+  if (!userAgent) {
+    return "bun"
+  }
+
+  const pkgSpec: any = userAgent.split(' ')[0]
+  const pkgSpecArr = pkgSpec.split('/')
+
+  return pkgSpecArr[0]
+}
+
+/**
+ * Path dir is empty
+ */
+function isEmpty(path: string) {
+  const files = readdirSync(path)
+  return files.length === 0 || (files.length === 1 && files[0] === '.git')
+}
+
+/**
+ * MAIN
+ */
 main(process.argv.slice(2))
